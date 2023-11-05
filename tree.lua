bfaf9e515cd3c8699d1f1e3dfbdc6e1fd84581bb
SugarCanedGames <149927962+SugarCanedGames@users.noreply.github.com>
2023-11-05 01:29:56 -0600

    Create tree.lua

---
+++ tree.lua
@@ -0,0 +1,101 @@
+return {
+    ["tree"]= "Default",
+    ["classes"]= {
+        {
+            ["name"]= "Scion",
+            ["base_str"]= 20,
+            ["base_dex"]= 20,
+            ["base_int"]= 20,
+            ["ascendancies"]= {
+                {
+                    ["id"]= "Ascendant",
+                    ["name"]= "Ascendant"
+                }
+            }
+        },
+        {
+            ["name"]= "Marauder",
+            ["base_str"]= 32,
+            ["base_dex"]= 14,
+            ["base_int"]= 14,
+            ["ascendancies"]= {
+                {
+                    ["id"]= "Juggernaut",
+                    ["name"]= "Juggernaut",
+                    ["flavourText"]= "     What divides the conqueror \n from the conquered? Perseverance.",
+                    ["flavourTextColour"]= "af5a32",
+                    ["flavourTextRect"]= {
+                        ["x"]= 215,
+                        ["y"]= 165,
+                        ["width"]= 1063,
+                        ["height"]= 436
+                    }
+                },
+                {
+                    ["id"]= "Berserker",
+                    ["name"]= "Berserker",
+                    ["flavourText"]= "The savage path is \nalways swift and sure.",
+                    ["flavourTextColour"]= "af5a32",
+                    ["flavourTextRect"]= {
+                        ["x"]= 760,
+                        ["y"]= 345,
+                        ["width"]= 976,
+                        ["height"]= 429
+                    }
+                },
+                {
+                    ["id"]= "Chieftain",
+                    ["name"]= "Chieftain",
+                    ["flavourText"]= "   The Ancestors speak \nthrough your clenched fists.",
+                    ["flavourTextColour"]= "af5a32",
+                    ["flavourTextRect"]= {
+                        ["x"]= 185,
+                        ["y"]= 245,
+                        ["width"]= 1076,
+                        ["height"]= 449
+                    }
+                }
+            }
+        },
+        {
+            ["name"]= "Ranger",
+            ["base_str"]= 14,
+            ["base_dex"]= 32,
+            ["base_int"]= 14,
+            ["ascendancies"]= {
+                {
+                    ["id"]= "Raider",
+                    ["name"]= "Raider",
+                    ["flavourText"]= "No hunt is complete without\nthe gutting and the skinning.",
+                    ["flavourTextColour"]= "7cb376",
+                    ["flavourTextRect"]= {
+                        ["x"]= 365,
+                        ["y"]= 965,
+                        ["width"]= 900,
+                        ["height"]= 1250
+                    }
+                },
+                {
+                    ["id"]= "Deadeye",
+                    ["name"]= "Deadeye",
+                    ["flavourText"]= "A woman can change the world \nwith a single well-placed arrow.",
+                    ["flavourTextColour"]= "7cb376",
+                    ["flavourTextRect"]= {
+                        ["x"]= 335,
+                        ["y"]= 1045,
+                        ["width"]= 870,
+                        ["height"]= 1330
+                    }
+                },
+                {
+                    ["id"]= "Pathfinder",
+                    ["name"]= "Pathfinder",
+                    ["flavourText"]= "There are venoms and virtues aplenty in \n the wilds, if you know where to look.",
+                    ["flavourTextColour"]= "7cb376",
+                    ["flavourTextRect"]= {
+                        ["x"]= 265,
+                        ["y"]= 975,
+                        ["width"]= 900,
+                        ["height"]= 1250
+                    }
+... (5 MB left)
